6f64c751efe87dce8cc3e87c08d2df100ac7c769
Fawad Shaikh <fawad@getmeintheinbox.com>
2023-06-18 18:49:07 -0400

    move assets to root directory

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,6 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    outDir: '../'
+    outDir: '../',
+    assetsDir: './'
   }
 })
